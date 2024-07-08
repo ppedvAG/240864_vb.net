166d94db4373372109e359b6513d280b281b6591
André <AndreR@ppedv.de>
2024-07-08 16:49:01 +0200

    Die schönste App der Welt mit FOR-Schleifen

---
+++ HalloSchleifenUndWinForms/HalloSchleifenUndWinForms/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Salmon
+        Me.Button1.Font = New System.Drawing.Font("Ravie", 19.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Lime
+        Me.Button1.Location = New System.Drawing.Point(16, 25)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(7)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(490, 127)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "FOR Schleife"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.Plum
+        Me.ListBox1.Font = New System.Drawing.Font("Lucida Calligraphy", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.Teal
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 48
+        Me.ListBox1.Location = New System.Drawing.Point(36, 195)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(570, 292)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.YellowGreen
+        Me.Button2.Font = New System.Drawing.Font("Matura MT Script Capitals", 16.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Magenta
+        Me.Button2.Location = New System.Drawing.Point(542, 37)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(426, 69)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Lese Listbox (for)"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.YellowGreen
+        Me.Button3.Font = New System.Drawing.Font("Harlow Solid Italic", 22.125!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Magenta
+        Me.Button3.Location = New System.Drawing.Point(612, 112)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(593, 102)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Lese Listbox (foreach)"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 29.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.ClientSize = New System.Drawing.Size(1259, 548)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ HalloSchleifenUndWinForms/HalloSchleifenUndWinForms/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        For index = 0 To 10 Step 3
+            ListBox1.Items.Add($"For: {index}")
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim text As String
+
+        For index = 0 To ListBox1.Items.Count - 1
+
+            Dim listBoxEintrag = ListBox1.Items(index)
+
+            If listBoxEintrag.ToString().EndsWith("0") Then
+                text += $"Zeile: {index} = {listBoxEintrag}{vbNewLine}"
+            End If
+        Next
+
+        MessageBox.Show(text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        For Each item In ListBox1.Items
+            MessageBox.Show(item)
+        Next
+
+    End Sub
+End Class
