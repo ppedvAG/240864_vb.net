efd8aa633afed8d5495b82af598edf6eeafc69fa
André <AndreR@ppedv.de>
2024-07-09 10:42:25 +0200

    While Dialoge Enums und Zeug mit WinForms

---
+++ HalloDateien/HalloDateien/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        TextBox1 = New TextBox()
+        SaveFileDialog1 = New SaveFileDialog()
+        FlowLayoutPanel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.AutoSize = True
+        FlowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        FlowLayoutPanel1.BackColor = Color.Olive
+        FlowLayoutPanel1.Controls.Add(Button1)
+        FlowLayoutPanel1.Controls.Add(Button2)
+        FlowLayoutPanel1.Controls.Add(Button3)
+        FlowLayoutPanel1.Controls.Add(Button4)
+        FlowLayoutPanel1.Dock = DockStyle.Top
+        FlowLayoutPanel1.Location = New Point(0, 0)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(1212, 48)
+        FlowLayoutPanel1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.AutoSize = True
+        Button1.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button1.BackColor = Color.DarkOrange
+        Button1.Location = New Point(3, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(237, 42)
+        Button1.TabIndex = 0
+        Button1.Text = "👙 Ein toller Button"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.AutoSize = True
+        Button2.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button2.BackColor = Color.SpringGreen
+        Button2.Image = My.Resources.Resources.disk_space
+        Button2.Location = New Point(246, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(202, 42)
+        Button2.TabIndex = 1
+        Button2.Text = "💾 Speichern"
+        Button2.TextImageRelation = TextImageRelation.ImageBeforeText
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.AutoSize = True
+        Button3.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button3.BackColor = Color.HotPink
+        Button3.Location = New Point(454, 3)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(188, 42)
+        Button3.TabIndex = 2
+        Button3.Text = "👁️ Lesen Datei"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.AutoSize = True
+        Button4.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        Button4.BackColor = Color.DodgerBlue
+        Button4.Location = New Point(648, 3)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(109, 42)
+        Button4.TabIndex = 3
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Dock = DockStyle.Fill
+        TextBox1.Location = New Point(0, 48)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(1212, 491)
+        TextBox1.TabIndex = 1
+        ' 
+        ' SaveFileDialog1
+        ' 
+        SaveFileDialog1.FileName = "blööö.txt"
+        SaveFileDialog1.Filter = "Texxxttt|*.txt"
+        SaveFileDialog1.Title = "AAAAAAA"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1212, 539)
+        Controls.Add(TextBox1)
+        Controls.Add(FlowLayoutPanel1)
+        Name = "Form1"
+        Text = "Form1"
+        FlowLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+
+End Class
---
+++ HalloDateien/HalloDateien/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For index = 1 To 10
+            TextBox1.Text += $"Alles wird gut {index}{vbCrLf}"
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            File.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        'TextBox1.Text = File.ReadAllText("text.txt")
+        Dim opendlg = New OpenFileDialog()
+        opendlg.Filter = "Textkram|*.txt|Den rest|*.*"
+        If opendlg.ShowDialog() = DialogResult.OK Then
+
+            Dim sr = New StreamReader(opendlg.FileName)
+
+            While Not sr.EndOfStream
+
+                Dim line = sr.ReadLine()
+
+                TextBox1.Text += line + vbCrLf
+
+            End While
+            sr.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+
+    End Sub
+End Class
