e832ba9fb7ba9a61e7c6fd8f6eb3f06b58f94cbb
André <AndreR@ppedv.de>
2024-07-09 11:34:50 +0200

    App mit startpara

---
+++ StartParameter/StartParameter/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main(ParamArray args() As String)
+
+        Console.WriteLine("HALLOOOO")
+
+        For Each arg In args
+            Console.WriteLine(arg)
+        Next
+
+        Console.WriteLine("ENDE")
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ StartParameter/StartParameter/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("StartParameter")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("StartParameter")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1d22b5bf-944d-4515-a0a5-633ea9b1d56f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
