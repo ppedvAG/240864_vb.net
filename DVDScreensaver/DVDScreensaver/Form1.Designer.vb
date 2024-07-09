e1ae64233ca1cd3caedcb6d4ac695107be1d3cfc
André <AndreR@ppedv.de>
2024-07-09 16:59:47 +0200

    DVD Screensaver

---
+++ DVDScreensaver/DVDScreensaver/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Crimson
+        Button1.Location = New Point(399, 224)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(192, 123)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        ClientSize = New Size(1374, 685)
+        Controls.Add(Button1)
+        ForeColor = SystemColors.ControlLightLight
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ DVDScreensaver/DVDScreensaver/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+
+    Dim speedY As Integer = 6
+    Dim speedX As Integer = 6
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SwitchYDirection()
+        SwitchXDirection()
+    End Sub
+
+    Sub SwitchYDirection()
+        speedY = speedY * -1
+        ChangeColor()
+    End Sub
+
+
+    Private Sub ChangeColor()
+        Task.Run(Sub() Console.Beep(220, 400))
+        Dim ran = New Random()
+        Button1.BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255))
+    End Sub
+
+    Sub SwitchXDirection()
+        speedX = speedX * -1
+        ChangeColor()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Button1.Left += speedY
+        Button1.Top += speedX
+
+        If Button1.Left < 0 Or (Button1.Left + Button1.Width) > ClientSize.Width Then
+            SwitchYDirection()
+        End If
+
+        If Button1.Top < 0 Or Button1.Bottom > ClientSize.Height Then
+            SwitchXDirection()
+        End If
+
+    End Sub
+End Class
